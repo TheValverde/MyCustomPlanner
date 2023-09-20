1a3a09e19081f4acffd19c6b6a81f29c02431c52
Hugo Valverde <hvalverde@quinceimaging.com>
2023-09-20 02:11:52 -0400

    init

---
+++ LifePilot/LifePilot/LifePilotApp.swift
@@ -0,0 +1,20 @@
+//
+//  LifePilotApp.swift
+//  LifePilot
+//
+//  Created by Hugo Valverde on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LifePilotApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ LifePilot/LifePilot/WelcomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreen.swift
+//  LifePilot
+//
+//  Created by Hugo Valverde on 9/20/23.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreen()
+    }
+}
